28b6785f308d1311465f1c0e6bed6d5c11a07ec1
Shaun <shaun@Shauns-MacBook-Pro.local>
2026-05-04 19:48:25 +0100

    prepare backend for railway

--- server.js
+++ server.js
@@ -243,4 +243,8 @@ const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
